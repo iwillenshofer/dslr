308f0fea961a61458f03097a2319fef310b91362
Igor Willenshofer <iwillens@student.42sp.org.br>
2023-10-12 19:03:10 +0200

    created own pairplot

--- pair_plot.py
+++ pair_plot.py
@@ -1,19 +1,69 @@
 from tools import stats
-from pandas import read_csv
+from pandas import read_csv, DataFrame
 from matplotlib import pyplot as plt
-import seaborn as sns
-
-sns.set()
 
 DATASET = "datasets/dataset_train.csv"
 
+# Defines the index column.
+# may be set as the column name, 0 for no index, or None, for auto number\
 INDEX_COL = 'Index'
 
 LABEL = 'Hogwarts House'
+
+FEATURES = [
+    'Arithmancy',
+    'Astronomy',
+    'Herbology',
+    'Defense Against the Dark Arts',
+    'Divination',
+    'Muggle Studies',
+    'Ancient Runes',
+    'History of Magic',
+    'Transfiguration',
+    'Potions',
+    'Care of Magical Creatures',
+    'Charms',
+    'Flying'
+]
+
+
+def pairplot(ds: DataFrame):
+    """creates a scatterplot taking dataframe as parameter"""
+    labels = ds[LABEL].unique()
+    nb_cols = len(FEATURES)
+    nb_rows = len(FEATURES)
+    fig, axs = plt.subplots(nb_rows, nb_cols)
+    for r, feat_row in enumerate(FEATURES):
+        for c, feat_col in enumerate(FEATURES):
+            for label in labels:
+                data1 = ds[ds[LABEL] == label][feat_row]
+                data2 = ds[ds[LABEL] == label][feat_col]
+                if r != c:
+                    axs[r][c].scatter(data1, data2, s=.1, alpha=.7)
+                else:
+                    axs[r][c].hist(data1, alpha=.5)
+                if not c:
+                    axs[r][c].set_ylabel(
+                        f'{feat_row}', fontsize=8, rotation=30, ha='right')
+                if not r:
+                    axs[r][c].set_title(
+                        f'{feat_col}', fontsize=8, rotation=30, ha='left')
+                if c != nb_cols - 1:
+                    axs[r][c].yaxis.set_tick_params(labelleft=False)
+                else:
+                    axs[r][c].yaxis.set_tick_params(
+                        labelleft=False, labelright=True)
+                if r != nb_rows - 1:
+                    axs[r][c].xaxis.set_tick_params(labelbottom=False)
+    fig.legend(labels, loc='lower right', markerscale=10.0)
+    fig.suptitle('Pairplot comparing all courses')
+    plt.show()
+
+
 try:
-    df = read_csv(DATASET, index_col=INDEX_COL)
-    stats.normalize_dataframe(df)
-    sns.pairplot(df, hue=LABEL)
+    ds = read_csv(DATASET, index_col=INDEX_COL)
+    stats.normalize_dataframe(ds)
+    pairplot(ds)
     plt.show()
 except Exception as error:
     print(f"{type(error).__name__}: {error}")
