d6c1af4b9ce14fe02dfcd526d9558cd4a033788a
iwillens <iwillenshofer@gmail.com>
2023-10-16 09:55:19 +0200

    working on gradient

--- logreg_train.py
+++ logreg_train.py
@@ -3,11 +3,8 @@
 # https://medium.com/analytics-vidhya/logistic-regression-from-scratch-multi-classification-with-onevsall-d5c2acf0c37c
 #
 
-import random
 import sys
 import math
-
-import numpy as np
 from tools import stats
 from pandas import read_csv, DataFrame
 from matplotlib import pyplot as plt
@@ -35,7 +32,7 @@ FEATURES = [
 LABEL = 'Hogwarts House'
 
 class LogisticRegression:
-    EPOCHS=1000
+    EPOCHS=3000
     LEARNING_RATE = 1
 
     def __init__(self, df: DataFrame, features: [str], label: str, graph=False):
@@ -45,10 +42,13 @@ class LogisticRegression:
         self.features = features;
         self.label = label
         self.classes = df[label].unique()
+        self.classes.sort()
         self.weight_matrix = [[0 for x in self.classes] for i in features]
+        self.learning_rate = self.LEARNING_RATE
+        self.epochs = self.EPOCHS
         #if graph:
         #    self.graph_sigmoid()
-        self.train()
+        weights = self.train()
         
     def graph_sigmoid(self):
         rng = [x / 10 for x in range(-50, 50)]
@@ -70,90 +70,68 @@ class LogisticRegression:
 
 
     def sigmoid(self, z: float):
-        return (1 / (1 + self.e ** -z))
+        value = 1 / (1 + self.e ** -z)
+        return (value)
 
 
-    def cost_function(self, data: DataFrame, feature: str):
-        X = list(data[feature])
-        y = list(data['class'])
 
-        a = sum([y[i] * math.log(self.sigmoid( X[i] * y[i] )) + (1 - y[i]) * math.log(1 - self.sigmoid(X[i] * y[i]))  for i, _ in enumerate(X)])
-        return (-1/len(X) * a)
-
-    def one_vs_all(self, feature: str, clas: str):
+    def cost_function(self, ys: [float], hs: [float]):
         """
-        returns a dataframe containing only the feature and label cols,
-        setting the selected class as 1 and all the others as 0.
-        nans are dropped.
+        Cost Function: 
+        J(θ) = -1/m * sum(y * log(h) + (1 - y) * log(1 - h))
+        J(θ) is the cost function that needs to be minimized during the training of logistic regression.
+        m is the number of training examples.
+        h is the predicted probability that example Xi belongs to the positive class (sigmod function) on x * class.
+        y is the actual label (0 or 1) for example Xi
         """
-        df = self.dataset[[feature, self.label]].copy()
-        df['class'] = 0
-        df.loc[df[self.label] == clas] = 1
-        df.drop(self.label, axis=1, inplace=True)
-        df.dropna(inplace=True)
-        return (df)
+        y_costs = [ys[i] * math.log(hs[i]) + (1 - ys[i]) * math.log(1 - hs[i]) for i, _ in enumerate(hs)]
+        cost = (-1 / len(ys)) * sum(y_costs)
+        return cost
 
-#   def gradient_descent(self, data, feature, clas):
-    def gradient_descent(self, data: DataFrame, feature: str, my_class: str):
-        X = list(data[feature])
-        y = list(data[my_class])
 
-        num_samples = len(X)
-        theta = np.zeros(2)  # Initialize parameters theta_0 and theta_1 to zero
 
-        for iteration in range(self.EPOCHS):
-            gradient_theta_0 = 0
-            gradient_theta_1 = 0
-
-            for i in range(num_samples):
-                # Calculate the prediction
-                prediction = self.sigmoid(theta[0] + theta[1] * X[i])
-
-                # Update the gradients
-                gradient_theta_0 += (prediction - y[i])
-                gradient_theta_1 += (prediction - y[i]) * X[i]
-
-            # Update the parameters using the gradients
-            theta[0] -= self.LEARNING_RATE * gradient_theta_0 / num_samples
-            theta[1] -= self.LEARNING_RATE * gradient_theta_1 / num_samples
-
-        return theta
-
-# bonus sohastic gradient...
-    def stochastic_gradient_descent(self, data: DataFrame, feature: str, my_class: str):
-        X = list(data[feature])
-        y = list(data[my_class])
-
-        num_samples = len(X)
-        theta = np.zeros(2)  # Initialize parameters theta_0 and theta_1 to zero
-
-        for iteration in range(self.EPOCHS):
-            # Randomly shuffle the data points at the beginning of each iteration
-            data_shuffled = list(zip(X, y))
-            random.shuffle(data_shuffled)
-
-            for i in range(num_samples):
-                xi, yi = data_shuffled[i]
-
-                # Calculate the prediction
-                prediction = self.sigmoid(theta[0] + theta[1] * xi)
-
-                # Update the gradients and parameters
-                gradient_theta_0 = prediction - yi
-                gradient_theta_1 = (prediction - yi) * xi
-
-                theta[0] -= self.LEARNING_RATE * gradient_theta_0
-                theta[1] -= self.LEARNING_RATE * gradient_theta_1
-
-        return theta
+    def one_vs_all(self, clas: str):
+        """
+        creates a new column called 'class' (if it does not exist)
+        sets all its values to 0
+        sets its value to 1 if its label row value == class
+        """
+        self.dataset['class'] = 0
+        self.dataset.loc[self.dataset[self.label] == clas, 'class'] = 1
 
 
     def train(self):
-        for feature in self.features:
-            for c in self.classes:
-                data = self.one_vs_all(feature, c)
-                print(data)
-                print(self.cost_function(data, feature))
+        """
+        for each class, create a one_vs_all and get the weights of its features
+        using a gradient descent
+        """
+        dic = {}
+        df = self.dataset[[LABEL] + FEATURES].copy()
+        for c in self.classes:
+            dic[c] = {}
+            self.one_vs_all(c)
+            #ratio = 1 / (stats.max(data[feature]) - stats.min(data[feature]))
+            #stats.normalize_dataframe(data)
+            weights = self.gradient_descent() #* ratio
+            dic[c] = weights
+        for key in dic:
+            print(dic[key])
+        return (dic)
+    
+
+    def gradient_descent(self):
+        import numpy as np
+        m = len(self.dataset)
+        thetas = np.random.rand(m)
+        for i in range(self.epochs):
+            z = [sum([thetas[j] * self.dataset[feature][i] for j, feature in enumerate(self.features)]) for i in range(m)]
+            h = [self.sigmoid(zz) for zz in z]
+            gradients = [sum([(h[i] -  self.dataset['class'][i]) * self.dataset[feature][i] for i in range(m)]) / m for _, feature in enumerate(self.features)]
+            thetas = [thetas[j] - self.learning_rate * gradients[j] for j, _ in enumerate(self.features)]
+            cost = self.cost_function(self.dataset['class'], h)
+            print(cost, thetas[0])
+        return thetas
+
 
 
 
@@ -172,11 +150,54 @@ def main():
     #try:
         validate_args()
         ds = read_csv(sys.argv[1], index_col=INDEX_COL)
-        print(stats.Describe(ds))
+        for feature in FEATURES:
+            ds[feature].fillna(stats.mean(ds[feature]), inplace=True)
+        stats.normalize_dataframe(ds)
+#        print(stats.Describe(ds))
         LogisticRegression(ds, FEATURES, LABEL, graph=True)
     #except Exception as error:
     #    print(f"{type(error).__name__}: {error}")
 
 
 if __name__ == "__main__":
+
+
     main()
+    exit()
+    # Import necessary libraries
+    import numpy as np
+    from sklearn.model_selection import train_test_split
+    import sklearn.linear_model as lm
+    from sklearn.metrics import accuracy_score, confusion_matrix
+
+    ds = read_csv("datasets/dataset_train.csv", index_col=INDEX_COL)
+    ds.fillna(0, inplace=True)
+    stats.normalize_dataframe(ds)
+    # Generate some random data for demonstration purposes
+    np.random.seed(42)
+    X = ds[FEATURES]
+    y = ds[[LABEL]]
+
+    model = lm.LogisticRegression(multi_class='ovr', solver='lbfgs')
+    model.fit(X, y)
+
+    # Make predictions on the test set
+    #y_pred = model.predict(X_test)
+
+    # Display intercept and weights for each class
+    print("Intercepts:", model.intercept_)
+    print("Weights for each class:")
+    print(model.classes_)
+    print(model.feature_names_in_)
+    print(model.coef_)
+    print(model)
+    # Evaluate the model
+    #accuracy = accuracy_score(y_test, y_pred)
+    #conf_matrix = confusion_matrix(y_test, y_pred)
+
+    # Print results
+    #print(f"Accuracy: {accuracy:.2f}")
+    #print("Confusion Matrix:")
+    #print(conf_matrix)
+
+
